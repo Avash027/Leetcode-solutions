cb65cc4c154abf674f6d5b052e4708427997857f
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-01-19 20:55:14 +0530

    Time: 1383 ms (9.93%), Space: 167.9 MB (5.01%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,23 +1,11 @@
 class Solution {
 private:
-    unordered_map<string,vector<string>> adj;
-   
+ 
+    unordered_map<string,bool> in; 
     unordered_map<string,int> dis;
   
     
-    void make_graph(string&cur , string&s){
-        
-            if(s.size()!=cur.size() or s==cur) return;
-            
-            int cnt = 0;
-            
-            for(int i = 0;i<s.size();i++) cnt+=(s[i]!=cur[i]);
-            if(cnt!=1) return;
-        
-            adj[s].push_back(cur);
-            adj[cur].push_back(s);
-        
-    }
+  
     
     void bfs(string src){
        
@@ -28,11 +16,21 @@ private:
         while(!q.empty()){
             string v = q.front();
             q.pop();
+            in.erase(v);
+            int d = dis[v];
             
-            for(string&u:adj[v]){
-                if(dis.count(u)!=0) continue;
-                dis[u] = dis[v]+1;
-                q.push(u);
+            for(int i = 0;i<v.size();i++){
+                for(char c = 'a';c<='z';c++){
+                    char temp = v[i];
+                    v[i] = c;
+                    
+                    if(in[v] and dis.count(v)==0){
+                        dis[v] = d+1;
+                        q.push(v);
+                    }
+                    
+                    v[i] = temp;
+                }
             }
         }
     }
@@ -40,18 +38,11 @@ private:
     
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        wordList.push_back(beginWord);
+        for(auto&e:wordList) in[e] = true;
         
-        for(int i = 0;i<wordList.size();i++)
-            for(int j = i+1;j<wordList.size();j++){
-                make_graph(wordList[i],wordList[j]);
-            }
         
         bfs(beginWord);
         
-        // for(string e:wordList){
-        //     cout<<e<<" "<<dis[e]<<"\n";    
-        // }
         
         
         return dis[endWord];

6d3ee1d9804fb67505b02d2b725b602f28c19852
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-04-03 11:39:17 +0530

    Time: 3 ms (52.49%), Space: 7.8 MB (24.89%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -12,6 +12,13 @@ private:
 public:
     int rob(vector<int>& nums) {
         vector<int> dp(nums.size()+1 , -1);
-        return maxAns(nums,(int)nums.size() , dp);    
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for(int i =2;i<=nums.size();i++)
+            dp[i] = max(nums[i-1] + dp[i-2] , dp[i-1]);
+        
+        return dp.back();
+        
     }
 };

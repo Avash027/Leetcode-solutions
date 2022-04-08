97ff7a265fa1e6f423e35d35cc9030a225b7cd08
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-04-08 06:50:00 +0530

    Time: 33 ms (19.24%), Space: 12.1 MB (5.29%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,38 +1,20 @@
 class Solution {
 private:
-    bool isPossible(string&s ,int k, char c, int len){
-        int cnt = 0;
-        bool ok = 0;
-        
-        for(int i = 0;i<len;i++)
-            cnt+=(s[i]!=c);
-        
-        ok|=(cnt<=k);
-        
-        if(ok) return true;
-        
-        for(int i = len;i<s.size();i++){
-            cnt-=(s[i-len]!=c);
-            cnt+=(s[i]!=c);
-            
-            ok|=(cnt<=k);
-            
-            if(ok) return true;
-        }
-        
-        return false;
-    }
-    
     int maxLen(string s,int k,char c){
-        int lo = 0 , hi = s.size();
-        int ans = 0;
+        int ans = 0,cnt = 0;
+        int lo = 0 , hi = 0;
         
-        while(lo<=hi){
-            int mid = lo + (hi-lo)/2;
-            if(isPossible(s,k,c,mid)) ans = mid , lo = mid+1;
-            else hi = mid-1;
+        while(hi<(int)s.size()){
+            cnt+=(s[hi++]!=c);
+            
+            while(cnt>k)
+                cnt-=(s[lo++]!=c);
+            
+            ans = max(ans , hi-lo);
         }
+        
         return ans;
+        
     }
 public:
     int characterReplacement(string s, int k) {

6306e38f6597f486d8283dc687f30161f5a5e47b
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-04-22 22:37:38 +0530

    Time: 149 ms (73.87%), Space: 61.1 MB (52.13%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -0,0 +1,49 @@
+class MyHashMap {
+	vector<vector<pair<int, int>>> map;
+	const int size = 10000;
+public:
+	MyHashMap() {
+		map.resize(size);
+	}
+
+	void put(int key, int value) {
+		int index = key % size;
+        vector<pair<int, int>> &row = map[index];
+        for(auto iter = row.begin(); iter != row.end(); iter++)
+        {
+            if(iter->first == key)
+            {
+                iter->second = value;
+                return;
+            }
+        }
+		row.push_back(make_pair(key, value));
+	}
+
+	int get(int key) {
+		int index = key % size;
+        vector<pair<int, int>> &row = map[index];
+		for (auto iter = row.begin(); iter != row.end(); iter++)
+		{
+			if (iter->first == key)
+			{
+				return iter->second;
+			}
+		}
+		return -1;
+	}
+
+
+	void remove(int key) {
+		int index = key % size;
+        vector<pair<int, int>> &row = map[index];
+		for (auto iter = row.begin(); iter != row.end(); iter++)
+		{
+			if (iter->first == key)
+			{
+				row.erase(iter);
+                return;
+			}
+		}
+	}
+};

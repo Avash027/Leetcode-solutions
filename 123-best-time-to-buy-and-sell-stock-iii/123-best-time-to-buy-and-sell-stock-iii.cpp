1fac8fc6d0e20cd96806dc8beb3a17e5385f5583
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-05-26 09:59:57 +0530

    Time: 1364 ms (5.01%), Space: 216.2 MB (17.09%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    vector<vector<vector<int>>> dp;
+    
+    
+    int profit(vector<int>& prices,int ind, bool canBuy , int numOfTr){
+        if(ind == prices.size() || numOfTr == 0) 
+            return 0;
+        
+        int p = 0;
+        
+        if(dp[ind][canBuy][numOfTr]!=-1) return dp[ind][canBuy][numOfTr];
+        
+        if(canBuy)
+            p = max(-prices[ind] + profit(prices,ind+1,false,numOfTr) , 
+                            0 + profit(prices,ind+1,true,numOfTr)
+                        );
+        else
+            p = max(prices[ind] + profit(prices,ind+1,true,numOfTr-1) , 
+                            0 + profit(prices,ind+1,false,numOfTr)
+                        );
+        
+        return dp[ind][canBuy][numOfTr] = p;
+    }
+    
+public:
+    int maxProfit(vector<int>& prices) {
+        const int n = prices.size();
+        
+        dp.assign(n , vector<vector<int>>(2 , vector<int>(3, -1)));
+        
+        return profit(prices,0,true,2);
+    }
+};

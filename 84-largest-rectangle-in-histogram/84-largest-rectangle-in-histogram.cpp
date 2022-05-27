2604c3ac461121439839f9cd0dd9e6f2f19a2998
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-05-27 09:46:15 +0530

    Time: 181 ms (86.40%), Space: 75.2 MB (96.99%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,49 +1,27 @@
 class Solution {
 private:
-        
-    int maxHeight(vector<int>&arr){
-        int n = arr.size();
-        stack<int> st;
-        vector<int> nxt(n,n) , prev(n,-1);
-        
-        for(int i = 0;i<n;i++){
-            while(!st.empty() and arr[i] < arr[st.top()]){
-                nxt[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        
-        while(!st.empty()) st.pop();
-        
-        for(int i = n-1;i>=0;i--){
-            while(!st.empty() and arr[i]<arr[st.top()]){
-                prev[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        
-        int ans = 0;
-        
-                                                                                                             
-        
-        
-        for(int i = 0;i<n;i++){
-            int left = i - prev[i]-1;
-            int right = nxt[i] - i-1;
-            
-            
-            
-            ans = max(ans , arr[i]*(left + right+1));
-        }
-        
-        return ans;
-    }
+     int maxArea(vector<int>&arr , int n){
+         stack<int> st;
+         int ans = 0;
+         for(int i = 0;i<=n;i++){
+             while(!st.empty() and (i == n || arr[st.top()] >= arr[i])){
+                 int h = arr[st.top()];
+                 st.pop();
+                 int w = 0;
+                 if(st.empty()) w = i;
+                 else w = i-st.top()-1;
+                 
+                 
+                 ans = max(ans , h*w);
+             }
+             st.push(i);
+         }
+         
+         return ans;
+     }
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int ans = maxHeight(heights);
-        
-        return ans;
+    int largestRectangleArea(vector<int>& arr) {
+        return maxArea(arr , arr.size());
+    
     }
 };

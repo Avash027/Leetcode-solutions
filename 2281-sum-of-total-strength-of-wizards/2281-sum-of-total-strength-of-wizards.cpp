65eb35cc09a2704349408f9a23dd34bc0efa1fde
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-05-28 08:15:14 +0530

    Time: 385 ms (38.49%), Space: 113.8 MB (12.64%) - LeetHub

---
+++ 2281-sum-of-total-strength-of-wizards/2281-sum-of-total-strength-of-wizards.cpp
@@ -0,0 +1,51 @@
+constexpr long long MOD = 1000000007;
+
+class Solution {
+public:
+    int totalStrength(vector<int>& arr) {
+        const int n = arr.size();
+        vector<long long> nxt(n,n) , prev(n,-1);
+        vector<long long> pre(n+1 , 0) , pre1(n+2 , 0);
+        stack<long long> st;
+        
+        for(int i = 0;i<n;i++)
+            pre[i+1] = (pre[i] + arr[i])%MOD;
+        
+        for(int i= 0;i<=n;i++)
+            pre1[i+1] = (pre1[i] + pre[i])%MOD;
+        
+        for(int i = 0;i<n;i++){
+            while(!st.empty() and arr[i] <= arr[st.top()]){
+                st.pop();
+            }
+            
+            prev[i] = st.empty()?-1:st.top();
+            
+            st.push(i);
+        }
+        
+        while(!st.empty()) st.pop();
+        
+        for(int i = n-1;i>=0;i--){
+            while(!st.empty() and arr[i]<arr[st.top()]){
+                st.pop();
+            }
+            nxt[i] = st.empty() ? n:st.top();
+            st.push(i);
+        }
+        
+        long long ans = 0;
+        
+        for(int i = 0;i<n;i++){
+            long long pos = ((pre1[nxt[i]+1] - pre1[i+1])%MOD * (i - prev[i])%MOD)%MOD;
+            long long neg = ((pre1[i+1] - pre1[prev[i]+1])%MOD *(nxt[i]-i)%MOD)%MOD;
+            
+            ans = (ans + ((pos - neg +MOD)%MOD*arr[i]%MOD)%MOD + MOD)%MOD;
+        }
+        ans%=MOD;
+        
+        return (int)ans;
+        
+        
+    }
+};

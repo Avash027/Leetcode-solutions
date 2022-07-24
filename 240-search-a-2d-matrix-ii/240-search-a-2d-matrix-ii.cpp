25dbfb11c4b8a5787a39a8ffecd2047f5b4f9de1
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-07-24 09:48:29 +0530

    Time: 468 ms (10.45%), Space: 14 MB (99.94%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    private:
+    
+    
+    bool findValue(const vector<int>&arr , int target){
+        ios::sync_with_stdio(0);
+        int lo = 0 , hi = (int)arr.size()-1;
+        
+        while(lo<=hi)
+        {
+            int mid = (lo + hi)>>1;
+            if(arr[mid] == target) return true;
+            if(target>arr[mid]) lo = mid+1;
+            else hi = mid-1;
+        }
+        return false;
+    }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i = 0;i<(int)matrix.size();i++)
+            if(findValue(matrix[i] , target)) return true;
+        
+        return false;
+    }
+};

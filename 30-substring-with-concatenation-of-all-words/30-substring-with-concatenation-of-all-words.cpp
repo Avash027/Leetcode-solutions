65fc4e4a362fbddb6027bf0a330d94073671daa5
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-08-13 17:41:18 +0530

    Time: 827 ms (36.13%), Space: 286.2 MB (27.80%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -45,15 +45,4 @@ public:
     }
 };
 
-  // for(int i = 0; i < n - total * len + 1; i++){
-  //           int j = i;
-  //           unordered_map<string, int> stats = freq;
-  //           int covered = 0;
-  //           while(covered < total && j + len - 1 < n) {
-  //               string word = s.substr(j, len);
-  //               if(stats.find(word) != stats.end() && --stats[word] >= 0) { 
-  //                   covered++;
-  //                   j += len;
-  //               }else break;
-  //           }
-  //           if(covered == total) answer.push_back(i);
+ 

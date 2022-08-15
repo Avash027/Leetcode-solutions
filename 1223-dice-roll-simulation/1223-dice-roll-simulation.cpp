33e69ae5593e2ec97097e0e647bf0d5986bd83e3
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-08-16 05:17:34 +0530

    Time: 74 ms (77.66%), Space: 9.3 MB (78.89%) - LeetHub

--- 1223-dice-roll-simulation/1223-dice-roll-simulation.cpp
+++ 1223-dice-roll-simulation/1223-dice-roll-simulation.cpp
@@ -1,30 +1,56 @@
 constexpr int MOD =1e9+7;
 
 class Solution {
-private:
+// private:
     
-    int dp[5010][8][17];
+//     int dp[5010][8][17];
     
-    int count(int n, vector<int>& rollMax,int clen,int last){
-        if(n == 0)
-            return 1;
+//     int count(int n, vector<int>& rollMax,int clen,int last){
+//         if(n == 0)
+//             return 1;
         
-        if(dp[n][last][clen] != -1)
-            return dp[n][last][clen];
+//         if(dp[n][last][clen] != -1)
+//             return dp[n][last][clen];
+        
+//         int ans = 0;
+        
+//         for(int i = 0;i<=5;i++){
+//             if(i+1 == last and clen == rollMax[i]) continue;
+//             ans += count(n-1,rollMax , (i+1 == last ? clen+1 :1) , i+1);
+//             ans%=MOD;
+//         }
+        
+//         return dp[n][last][clen] =  ans;
+//     }
+public:
+    int dieSimulator(int n, vector<int>& rollMax) {
+        int dp[n+1][7][16];
+        
+        memset(dp,0,sizeof(dp));
+        
+        
+        for(int i = 1;i<=6;i++)
+            dp[1][i][1] = 1;
+        
+        
+        for(int i = 2;i<=n;i++){
+            for(int j = 1;j<=6;j++) // current
+                for(int k = 1;k<=6;k++) // prev
+                    for(int l = 1;l<=15;l++){
+                        if(j == k and l+1<=rollMax[j-1])
+                            (dp[i][j][l+1]+= dp[i-1][j][l])%=MOD;
+                        else if(j!=k)
+                            (dp[i][j][1] += dp[i-1][k][l])%=MOD;
+                            
+                    }
+            }
         
         int ans = 0;
         
-        for(int i = 0;i<=5;i++){
-            if(i+1 == last and clen == rollMax[i]) continue;
-            ans += count(n-1,rollMax , (i+1 == last ? clen+1 :1) , i+1);
-            ans%=MOD;
-        }
+        for(int i = 1;i<=6;i++)
+            for(int l = 1;l<=15;l++)
+                (ans+=dp[n][i][l])%=MOD;
         
-        return dp[n][last][clen] =  ans;
-    }
-public:
-    int dieSimulator(int n, vector<int>& rollMax) {
-        memset(dp,-1,sizeof(dp));
-        return count(n,rollMax,0,0);
+        return ans;
     }
 };

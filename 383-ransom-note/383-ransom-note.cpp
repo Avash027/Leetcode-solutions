687c8758bce1a37edd90906f182cf30f2185ed66
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-08-25 17:49:40 +0530

    Time: 58 ms (9.51%), Space: 8.9 MB (15.41%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> freq;
+        
+        for(char&c:magazine)
+            freq[c]++;
+        
+        for(char&c:ransomNote){
+            freq[c]--;
+        
+            if(freq[c]<0)
+                return false;
+        }
+        
+        return true;
+    }
+};

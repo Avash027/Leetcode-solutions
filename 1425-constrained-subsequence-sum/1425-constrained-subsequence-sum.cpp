27ac1536542b25e17b91dd9258d33c3eaf14a4d2
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-09-02 06:44:35 +0530

    Time: 639 ms (27.61%), Space: 122.1 MB (54.25%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -1,3 +1,6 @@
+//Time Complexity : O(N) (Because we get maximum element in O(1))
+//Spcae complexity: O(N)
+
 class Solution
 {
     public:
@@ -11,32 +14,35 @@ class Solution
             */
             
             dp[0] = nums[0];
-            // We will use max_prev to store the previous k sum of subsequences
             
-            set<pair<int, int>> max_prev;
+            // dq will always store values in non-increasing (descending order)
+            //So dq.front() will have greatest value in the range
+            deque<int> dq;
+            dq.push_back(dp[0]);
             
-            max_prev.insert({ dp[0], 0 });
             int ans = dp[0];
 
             for (int i = 1; i < N; i++)
             {
                 
                 // We want elements in the range [i-k , i], since i-k-1 is out of bounds we remove it
-                if (i - k - 1 >= 0)
-                    max_prev.erase({ dp[i - k - 1],i - k - 1 });
+                if (i - k - 1 >= 0 and dq.front() == dp[i-k-1])
+                    dq.pop_front();
 
-                
-                int mx = max_prev.rbegin()->first; // We find the subsequence with maximum sum
 
                 // Here if we want to append nums[i] to the previous subsequence we will get sum mx + nums[i]
                 // If we want to start a new subsequence from nums[i] we will get nums[i]
                 // We take the best of two 
                 
-                dp[i] = max(mx + nums[i], nums[i]);
+                dp[i] = max(dq.front() + nums[i], nums[i]);
                 ans = max(ans, dp[i]);
                 
-                //We will use this value in the future
-                max_prev.insert({ dp[i],i });
+                //If dp[i] is greater then dq.back() then the values in dq wont be non-increasing, so we remove them
+                while(!dq.empty() and dp[i] > dq.back())
+                    dq.pop_back();
+                
+                dq.push_back(dp[i]);
+                
             }
 
             return ans;

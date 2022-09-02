115cfaf9f65a66e9bf16aed774ff97772ceede85
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-09-02 07:39:15 +0530

    Time: 543 ms (33.37%), Space: 102.8 MB (18.72%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.cpp
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -10,7 +10,7 @@ public:
 
         
         // store the elements in the order [yi - xi , xi]
-        //In monotonic dqueue
+        //In monotonic dqueue we make sure that [yi - xi] will always be non-increasing (descending)
         deque<pair<int,int>> dq;
         
         
@@ -23,12 +23,14 @@ public:
         for(int i = 1;i<N;i++){
             auto e = points[i];
             
+            //Since xi is increasing, we can be sure that xi  is arranged in non-decreasing order
             while(!dq.empty() and abs(e[0] - dq.front().second) > k)
                 dq.pop_front();
             
             if(!dq.empty()) 
                 ans = max(ans , e[0] + e[1] + dq.front().first);
             
+            //We keep removing the last elements as long as they are smaller than the current element. Once we are done we push it to the dq. So it remains non-increasing (descending)
             while(!dq.empty() and e[1] - e[0] > dq.back().first)
                 dq.pop_back();
             

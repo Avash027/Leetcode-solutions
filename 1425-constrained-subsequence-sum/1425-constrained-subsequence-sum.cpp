f711c20ff4d8c85e59b83c310018ea3299c02c15
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-09-02 06:35:30 +0530

    Time: 924 ms (11.85%), Space: 208.4 MB (13.11%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -1,35 +1,44 @@
-class Solution {
-public:
-    int constrainedSubsetSum(vector<int>& nums, int k) {
-        const int N = nums.size();
-        vector<int> dp(N);
-        
+class Solution
+{
+    public:
+        int constrainedSubsetSum(vector<int> &nums, int k)
+        {
+            const int N = nums.size();
+            vector<int> dp(N);
 
-        
-        
-        dp[0] = nums[0];
-        set<pair<int,int>> max_prev;
-        max_prev.insert({dp[0] , 0});
-        int ans = dp[0];
-        
-        for(int i = 1;i<N;i++){
+            /**
+            * dp[i] = Maximum sum of subsequence if we take the element at index i
+            */
             
-            if(i-k-1>=0)
-                max_prev.erase({dp[i-k-1] , i-k-1});
+            dp[0] = nums[0];
+            // We will use max_prev to store the previous k sum of subsequences
             
-            int mx = max_prev.rbegin()->first;
-            int mx_ind = max_prev.rbegin()->second;
+            set<pair<int, int>> max_prev;
             
-            dp[i] = max(mx + nums[i],nums[i]);
-            ans = max(ans , dp[i]);
-            max_prev.insert({dp[i],i});
-            
+            max_prev.insert({ dp[0], 0 });
+            int ans = dp[0];
+
+            for (int i = 1; i < N; i++)
+            {
+                
+                // We want elements in the range [i-k , i], since i-k-1 is out of bounds we remove it
+                if (i - k - 1 >= 0)
+                    max_prev.erase({ dp[i - k - 1],i - k - 1 });
+
+                
+                int mx = max_prev.rbegin()->first; // We find the subsequence with maximum sum
+
+                // Here if we want to append nums[i] to the previous subsequence we will get sum mx + nums[i]
+                // If we want to start a new subsequence from nums[i] we will get nums[i]
+                // We take the best of two 
+                
+                dp[i] = max(mx + nums[i], nums[i]);
+                ans = max(ans, dp[i]);
+                
+                //We will use this value in the future
+                max_prev.insert({ dp[i],i });
+            }
+
+            return ans;
         }
-        
-        
-        return ans;
-        
-        
-        
-    }
 };

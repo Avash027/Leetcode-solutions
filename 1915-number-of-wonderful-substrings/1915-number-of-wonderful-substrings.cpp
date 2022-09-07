196b95e02400a20f8087790e7fe4a049e1855c93
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-09-07 06:43:29 +0530

    Time: 1190 ms (21.66%), Space: 17.5 MB (42.96%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -3,25 +3,15 @@ public:
     long long wonderfulSubstrings(string word) {
         const int N = word.size();
         unordered_map<int,int> maskCnt;
-        vector<vector<int>> pre(N , vector<int>(10 , 0));
         
         
         long long ans = 0;
-        
+        int mask = 0;
         maskCnt[0] = 1;
  
         for(int i = 0;i<N;i++){
             
-            int mask = 0;
-            
-            for(char c = 'a';c<='j';c++){
-                pre[i][c-'a'] += (i > 0 ? pre[i-1][c-'a'] : 0) + (c == word[i]);
-            
-                if(pre[i][c-'a']&1)
-                    mask += (1<<(c-'a'));
-            
-            }
-
+            mask ^= (1<<(word[i]-'a'));
           
             ans+=maskCnt[mask];
             

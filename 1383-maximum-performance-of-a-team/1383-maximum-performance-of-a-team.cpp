3bb4a94a54ae31256e09b46e3fe90bd6be4355fc
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-09-11 20:03:41 +0530

    Time: 202 ms (18.31%), Space: 36.9 MB (56.16%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxPerformance(int n, vector<int> &speed, vector<int> &efficiency, int k) {
+        vector<pair<int, int>> engineers;
+        for (int i = 0; i < n; ++i)
+            engineers.emplace_back(efficiency[i], speed[i]);
+        
+        sort(rbegin(engineers), rend(engineers)); // Sort by decreasing order of efficiency
+        long speedSum = 0, ans = 0;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for (auto&[e, s] : engineers) {
+            speedSum += s;
+            minHeap.emplace(s);
+            if (minHeap.size() > k) { // Over k engineers -> remove the lowest speed engineer
+                speedSum -= minHeap.top();
+                minHeap.pop();
+            }
+            ans = max(ans, speedSum * e);
+        }
+        return ans % (int) (1e9 + 7);
+    }
+};

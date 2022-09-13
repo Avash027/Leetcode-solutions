635f84c577891788f1008c8dd654b7d76780449a
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-09-14 04:45:14 +0530

    Time: 107 ms (33.55%), Space: 14.8 MB (11.62%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -2,26 +2,36 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         const int N = s.size();
-        vector<set<int>> ls(3);
+        vector<vector<int>> ls(3);
         
         for(int i = 0;i<N;i++)
-            ls[s[i]-'a'].insert(i);
+            ls[s[i]-'a'].push_back(i);
         
-        ls[0].insert(N+1);
-        ls[1].insert(N+1);
-        ls[2].insert(N+1);
+        ls[0].push_back(N+1);
+        ls[1].push_back(N+1);
+        ls[2].push_back(N+1);
+        
+        int ind_a =0, ind_b = 0 , ind_c = 0;
         
         int ans = 0;
         
         for(int i = 0;i<N;i++){
-            int ind_a = *ls[0].lower_bound(i);
-            int ind_b = *ls[1].lower_bound(i);
-            int ind_c = *ls[2].lower_bound(i);
             
-            if(max({ind_a,ind_b,ind_c})>N)
+            while(ind_a<ls[0].size() and ls[0][ind_a]<i)
+                ind_a++;
+            
+            while(ind_b<ls[1].size() and ls[1][ind_b]<i)
+                ind_b++;
+            
+            while(ind_c<ls[2].size() and ls[2][ind_c]<i)
+                ind_c++;
+            
+            int last_ind = max({ls[0][ind_a] , ls[1][ind_b], ls[2][ind_c]});
+            
+            if(last_ind>=N)
                 break;
             
-            ans += N - max({ind_a,ind_b,ind_c});
+            ans += N - last_ind;
         }
         
         return ans;
